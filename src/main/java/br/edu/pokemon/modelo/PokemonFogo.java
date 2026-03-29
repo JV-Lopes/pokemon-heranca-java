ca124364a803e7f6f35bcef82fc093a68a625d5d
José Vieira <jn038576@gmail.com>
2026-03-29 15:26:58 -0300

    feat: adiciona superclasse Pokemon e sobclasse PokemonFogo

---
+++ src/main/java/br/edu/pokemon/modelo/PokemonFogo.java
@@ -0,0 +1,26 @@
+package main.java.br.edu.pokemon.modelo;
+
+public class PokemonFogo extends Pokemon {
+
+    private int temperaturaChama; // em graus Celsius
+
+    public PokemonFogo(String nome, int nivel, int hp, int temperaturaChama) {
+        super(nome, nivel, hp);
+        this.temperaturaChama = temperaturaChama;
+    }
+
+    @Override
+    public String atacar() {
+        return getNome() + " usa LANÇA-CHAMAS! 🔥 "
+             + "(Temperatura da chama: " + temperaturaChama + "°C)";
+    }
+
+    public int getTemperaturaChama()        { return temperaturaChama; }
+    public void setTemperaturaChama(int t)  { this.temperaturaChama = t; }
+
+    @Override
+    public String toString() {
+        return "PokemonFogo{nome='" + getNome() + "', nivel=" + getNivel()
+             + ", hp=" + getHp() + ", temperaturaChama=" + temperaturaChama + "}";
+    }
+}
